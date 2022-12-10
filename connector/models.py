7c0c6e4100250a9fcfee5b3f46813c289929cf9c
LW83 <ljbwilkinson@gmail.com>
2022-12-10 16:34:13 +0000

    Create Abstract User Model

--- connector/models.py
+++ connector/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_pound = models.BooleanField('pound status', default=False)
+    is_rescue = models.BooleanField('rescue status', default=False)
